093bb533b6e4f4e5a740bfbe326d77c8f2e03d4d
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-12 14:55:58 +0200

    Fix grid creation

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('#grid');
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     square.className = 'square';
+    square.style.width = 'calc((100% - 8px * 2 - 8px * 15) / 16)';
     grid.appendChild(square);
 }
 
@@ -11,17 +12,18 @@ document.addEventListener('mouseover', (e) => {
   }
 });
 
-let size;
+
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     grid.textContent = "";
-    size= parseInt(prompt('Pick a number from 1 to 100 to build your grid.\nExample: if you pick 10, you get a 10x10 grid with 100 squares total.'));
+    let size= parseInt(prompt('Pick a number from 1 to 100 to build your grid.\nExample: if you pick 10, you get a 10x10 grid with 100 squares total.'));
     if(size <= 0 || size > 100) {
         alert("We cannot create a grid with values lower or equal to 0 or bigger than 100");
     } else {
         for (let i = 0; i < size ** 2; i++) {
                 let square = document.createElement('div');
                 square.className = 'square';
+                square.style.width = `calc((100% - 8px * 2 - 8px * (${size} - 1)) / ${size})`;
                 grid.appendChild(square);
         }
     }
